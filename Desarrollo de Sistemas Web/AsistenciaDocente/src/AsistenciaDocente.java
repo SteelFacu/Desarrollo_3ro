6db51d004dae5506adf7826b3bd97812ff6b79bc
SteelFacu <facundo.melendrez456@gmail.com>
2025-05-27 20:02:09 -0300

    ultima modificacion 20:02

--- Desarrollo de Sistemas Web/AsistenciaDocente/src/AsistenciaDocente.java
+++ Desarrollo de Sistemas Web/AsistenciaDocente/src/AsistenciaDocente.java
@@ -1,16 +1,18 @@
 import java.sql.Time;
 
-public class AsistenciaDocente {
-    private Time horarioIngreso;
-    private Time horarioSalida;
-    private Docente docente;
+public class AsistenciaDocente extends Asistencia {
+    public Time horarioIngreso;
+    public Time horarioSalida;
+    public Docente docente;
 
     public AsistenciaDocente(Time horarioIngreso, Time horarioSalida, Docente docente) {
+        super();
         this.horarioIngreso = horarioIngreso;
         this.horarioSalida = horarioSalida;
         this.docente = docente;
     }
 }
+
 
 
 
