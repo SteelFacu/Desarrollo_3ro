cc1d88bf18e2340d3686a0ebc8182cfd8e3416c6
uwu <random.user@gmail.com>
2025-06-02 21:15:03 -0300

    Agregamos clase documento

---
+++ Desarrollo de Sistemas Web/AsistenciaDocente/src/Documento.java
@@ -0,0 +1,33 @@
+public class Documento {
+    private String nombre;
+    private String tipo; //por ej DNI, TITULO, CV
+    private String urlArchivo; //o ruta local
+
+    public Documento(String nombre, String tipo, String urlArchivo){
+        this.nombre=nombre;
+        this.tipo=tipo;
+        this.urlArchivo=urlArchivo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+   public void setNombre(String nombre) {
+       this.nombre = nombre;
+   }
+   public String getTipo(){
+        return tipo;
+   }
+   public void setTipo(String tipo){
+        this.tipo=tipo;
+   }
+
+    public void setUrlArchivo(String urlArchivo) {
+        this.urlArchivo = urlArchivo;
+    }
+
+    public String getUrlArchivo() {
+        return urlArchivo;
+    }
+
+}
